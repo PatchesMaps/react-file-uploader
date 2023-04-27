b477083c42ae28d3cfc68a72c09da604933fb4ee
Patrick Moulden <4834892+PatchesMaps@users.noreply.github.com>
2023-04-27 13:43:34 -0400

    Update FileInput.jsx

--- src/components/FileInput/FileInput.jsx
+++ src/components/FileInput/FileInput.jsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from 'react'
-// import PropTypes from 'prop-types'
-// import { ButtonGroupHeader, BrowseLink } from ...
 import { Attachment } from '../Attachment'
 import './FileInput.css'
 
